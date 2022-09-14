4945b59aa8cb2443a2c3b2d08d679c6e95147ac8
power4dyno <113491441+power4dyno@users.noreply.github.com>
2022-09-14 09:18:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,10 +50,10 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "<b>⚠️ Don't send me messages directly I'm only File Share bot ❕❗</b>"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(1070126339)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
